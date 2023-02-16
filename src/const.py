72ed3c31ac6ebbd16276fa987402458e68ce5c8f
pheni-18 <pheni.engineer@gmail.com>
2023-02-16 21:08:29 +0900

    add openpyxl

---
+++ src/const.py
@@ -0,0 +1,3 @@
+SHEET_NAME = '勤務表'
+
+NAME_CELL = (2, 7)
--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
+from const import SHEET_NAME, NAME_CELL
+from openpyxl import load_workbook
+
 import argparse
 import datetime
+import os
 
 
-def main():
+def read_args():
     parser = argparse.ArgumentParser(description='translator')
 
     parser.add_argument('file_path', help='target excel file')
@@ -11,21 +15,38 @@ def main():
 
     default_month = now.month
     month_choices = [i for i in range(1, 13)]
-    parser.add_argument('-m', '--month', type=int, default=default_month, choices=month_choices, help='target month')
+    parser.add_argument('-m', '--month', type=int, default=default_month,
+        choices=month_choices, help='target month')
 
     default_year = now.year
     year_choices = [i for i in range(1970, now.year + 2)]
-    parser.add_argument('-y', '--year', type=int, default=default_year, choices=year_choices, help='target year')
+    parser.add_argument('-y', '--year', type=int, default=default_year,
+        choices=year_choices, help='target year')
 
     args = parser.parse_args()
-
-    file_path = args.file_path
+    file_path = os.path.join(os.getcwd(), args.file_path)
     month = args.month
     year = args.year
+
+    return file_path, month, year
+
+
+def main():
+    file_path, month, year = read_args()
+
+    file_name, file_ext = os.path.splitext(os.path.basename(file_path))
 
     print(file_path)
     print(month)
     print(year)
+
+    print(file_name)
+    print(file_ext)
+
+    workbook = load_workbook(file_path)
+    sheet = workbook[SHEET_NAME]
+    name = sheet.cell(row=NAME_CELL[0], column=NAME_CELL[1]).value
+    print(name)
 
 
 if __name__ == '__main__':
